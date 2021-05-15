b839cfb7e84cd74bcc12bd88b859e03addf1f72b
maiconm <maiconm23@gmail.com>
2021-05-15 17:14:42 -0300

    Adiciona classe de utils

---
+++ src/main/java/com/delivery/api/utils/Utils.java
@@ -0,0 +1,34 @@
+package com.delivery.api.utils;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import org.springframework.util.ReflectionUtils;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Utils {
+	
+	public static void merge(Object original, Map<String, Object> campos) {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+		
+		Object origem = objectMapper.convertValue(campos, original.getClass());
+		
+		campos.forEach((propriedade, valor) -> {
+			
+			Field campo = ReflectionUtils.findField(original.getClass(), propriedade);
+			campo.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(campo, origem);
+			
+			ReflectionUtils.setField(campo, original, novoValor);
+			
+		});
+		
+	}
+	
+}
