d290295b847be22e2f299eff9261b225f81a73c0
maiconm <maiconm23@gmail.com>
2021-05-15 16:45:57 -0300

    Adiciona classes Restaurante e Produto com seus relacionamentos

---
+++ src/main/java/com/delivery/api/entity/Restaurante.java
@@ -0,0 +1,45 @@
+package com.delivery.api.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "restaurante")
+public class Restaurante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+
+	@Column(name = "taxa_frete")
+	private BigDecimal taxaFrete;
+
+	private String cep;
+
+	private String rua;
+
+	private String numero;
+
+	private String bairro;
+
+	private String complemento;
+
+	@OneToMany
+	@JoinColumn(name = "id_restaurante")
+	private List<Produto> produtos;
+
+}
