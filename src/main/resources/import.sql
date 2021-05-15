ce32225f31553527a1f99546f029d8e18efbe3d0
maiconm <maiconm23@gmail.com>
2021-05-15 16:20:05 -0300

    Configuração inicial

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into restaurante (nome, taxa_frete, bairro, numero) values ("Madero", 10.0, "Cidade Jardim", 123);
+insert into restaurante (nome, taxa_frete, bairro, numero) values ("Pentucci", 15.0, "Alphavile", 456);
+insert into restaurante (nome, taxa_frete, bairro, numero) values ("MacDonalds", 13.0, "Boqueirão", 789);
+
+insert into produto (descricao, nome, preco, uuid, id_restaurante) values ("teste descrição", "teste nome", 7.77, "c26e7da3-ec11-4ebf-917c-cf76798bce5a", 1);
