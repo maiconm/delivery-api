c0a449f4fb7228fd0a958fb59d9223da4a54d214
maiconm <maiconm23@gmail.com>
2021-05-15 20:39:12 -0300

    Exemplo CSRF e inMemoryAuthentication

--- src/main/java/com/delivery/api/security/WebSecurityConfiguration.java
+++ src/main/java/com/delivery/api/security/WebSecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.delivery.api.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +18,21 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.and()
 			.authorizeRequests()
 				.antMatchers("/utils/**").permitAll()
-				.anyRequest().authenticated();
+				.anyRequest().authenticated()
+			.and()
+				.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.csrf().disable();
 	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("maicon")
+			.password("123456")
+			.roles("ADMIN");
+	}
+	
 	
 }
